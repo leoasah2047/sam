1252c5adc41e108d341b9ead273ba807c0feef9d
leoasah2047 <76005372+leoasah2047@users.noreply.github.com>
2021-09-10 19:32:06 +0100

    Update settings.py

--- foodstore/settings.py
+++ foodstore/settings.py
@@ -61,6 +61,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_files')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
